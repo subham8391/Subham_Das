def51c3113d35178089284dbcc90f2ee1a006525
i-subham <i-subham@thesys.dev>
2024-08-17 22:35:39 +0530

    Navbar ,glob,techstack

---
+++ src/Components/Grid.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/Data";
+
+const Grid = () => {
+  return (
+    <section id="About">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            imgClassName,
+            titleClassName,
+            img,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              key={id}
+              id={id}
+              title={title}
+              description={description}
+              className={className}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              img={img}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Hero from "@/Components/Hero";
+import Grid from "@/Components/Grid";
 import { FloatingNav } from "@/Components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={[{name:'Home',link:'/',icon:<FaHome/>}]}/>
         <Hero />
+        <Grid />
       </div>
      
     </main>
